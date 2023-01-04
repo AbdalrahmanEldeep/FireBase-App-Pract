b053b285197f0af63b8d6ad8e816f9afc21d889a
unknown <abdo.eslam2021@gmail.com>
2023-01-04 17:25:24 +0200

    UPPEND USER INFO

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './style.css'
-import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged ,updateProfile, signOut } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 
 
@@ -20,8 +20,70 @@ const provider = new GoogleAuthProvider();
 
 const auth = getAuth();
 
-document.querySelector(".sign").addEventListener("click",() =>{
-  signInWithPopup(auth, provider)
+let userName,email,photo,status,Log = "Login";
+
+
+window.addEventListener("DOMContentLoaded",() =>{
+  appendElements();
+})
+
+
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    email = user.email;
+    userName = user.displayName;
+    photo = user.photoURL;
+    status = user.emailVerified;
+    Log ="Logout";
+    appendElements(); 
+  } else {
+      email = "";
+      userName = "";
+      photo = "";
+      status ="";
+      Log ="Login";
+      appendElements(); 
+      console.log('There\'s not users')
+  }
+});
+
+
+function appendElements(){
+ document.querySelector('#app').innerHTML = `
+  <div class="app-container">
+    <div class ="side-bare">
+        <div class="container">
+          <div class="info">
+              <div class="user-photo"><img src="${photo}" alt /></div>
+                <div class ="user-name-email">
+                  <div class="inp-box">
+                     <input disabled value= ${userName ? userName :""}>
+                     <p>Edit</p> 
+                  </div>
+                  <div class="inp-box">
+                    <input disabled value=${email? email : ""}>
+                    <p>Edit</p>
+                  </div>
+              </div>
+          </div>
+        </div>
+    </div>
+    <form>
+      <button class="login">${Log}</button>
+    </form>
+  </div>
+`
+document.querySelector(".login").addEventListener("click",(e) =>{
+  e.preventDefault();
+  if(status){
+    signOut(auth).then(() => {
+      // Sign-out successful.
+    }).catch((error) => {
+      // An error happened.
+    });
+  }else{
+    signInWithPopup(auth, provider)
     .then((result) => {
       // This gives you a Google Access Token. You can use it to access the Google API.
       const credential = GoogleAuthProvider.credentialFromResult(result);
@@ -37,19 +99,14 @@ document.querySelector(".sign").addEventListener("click",() =>{
       const email = error.customData.email;
       // The AuthCredential type that was used.
       const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
     });
 
+  }
 })
 
-document.querySelector('#app').innerHTML = `
-  <button class="sign">Login</button>
-`
+}
 
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    console.log(user.email);
-    console.log(user.photoURL);
-    console.log(user.displayName);
-  } else {
-  }
-});
+
+
+
